6aa699b9bf573d7314255645cfbc1fa7b9336905
ben <ben@jammerware.com>
2019-04-20 14:48:47 -0400

    updated project settings and added datalog prefab.

--- Assets/Scripts/Atalanta/Behaviours/KillOnPlayerCollide.cs
+++ Assets/Scripts/Atalanta/Behaviours/KillOnPlayerCollide.cs
@@ -6,7 +6,8 @@ namespace Atalanta.Behaviours
     {
         void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.CompareTag("Player")) {
+            if (other.CompareTag("Player"))
+            {
                 var audioSource = this.GetComponent<AudioSource>();
                 audioSource.Play();
 
@@ -14,6 +15,7 @@ namespace Atalanta.Behaviours
                 // we can't disable/destroy this object immediately, because it'll stop
                 // the audio source from playing. so we disable the renderer and collider
                 // and schedule the object for destruction
+                Debug.Log("got the audio source " + audioSource.clip.length);
                 GetComponent<SpriteRenderer>().enabled = false;
                 GetComponent<Collider2D>().enabled = false;
                 Destroy(this, audioSource.clip.length);
